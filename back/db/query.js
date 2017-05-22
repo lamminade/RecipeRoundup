3b6eb95fbdb3c5532d655b1913515b620e7b2520
Lance Zhou <lance@lances.space>
2017-05-22 15:43:28 -0400

    Initial server

--- back/db/query.js
+++ back/db/query.js
@@ -1,20 +1,26 @@
 var exports = module.exports = {};
 
-exports.mergeAndSearch = function(recipe) {
-	var q1 = "MERGE (a:Recipe {name: \"" + recipe.name + "\", url: \"" + recipe.url + "\"}) FOREACH (x in " + JSON.stringify(recipe.ingredients) + " | MERGE (i:Ingredient {name: x}) MERGE (a)-[:Contains]->(i)) RETURN null";
-	var q2 = "MATCH (a:Recipe {name: \"" + recipe.name + "\", url: \"" + recipe.url + "\"})-[:Contains]-(:Ingredient)-[:Contains]-(b:Recipe), (a)-[:Contains]->(ia:Ingredient), (b)-[:Contains]->(ib:Ingredient) RETURN b";
+exports.mergeAndSearch = function(recipe, callback) {
+	var q1 = "MERGE (a:Recipe {name: \"" + recipe.recipe + "\", url: \"" + recipe.url + "\"}) FOREACH (x in " + JSON.stringify(recipe.ingredients) + " | MERGE (i:Ingredient {name: x}) MERGE (a)-[:Contains]->(i)) RETURN null";
+	var q2 = "MATCH (a:Recipe {name: \"" + recipe.recipe + "\", url: \"" + recipe.url + "\"})-[:Contains]-(:Ingredient)-[:Contains]-(b:Recipe) RETURN DISTINCT b";
 	var request = require("request");
 	var reqBody = 
 		{ "json": {"statements": [
 			{ "statement" : q1 },
 			{ "statement" : q2 }
 		] }};
-	console.log(reqBody);
+	console.log(q1);
+	console.log(q2);
 	request.post("http://localhost:7474/db/data/transaction/commit",
 		reqBody, function(err, res, body) {
 			if (!err && res.statusCode == 200) {
-				return(body.results);
+				var nodes = [];
+				for (r of body.results[1].data) {
+					nodes.push(r.row[0]);
+				}
+				callback(null, {"nodes": nodes});
+			} else {
+				callback(err, res.statusCode);
 			}
 		}).auth("neo4j", "", false);
-	return null;
 }
--- back/serv.js
+++ back/serv.js
@@ -3,16 +3,26 @@ var url = require("url");
 
 var server = http.createServer(function(req, res) {
 	var link = url.parse(req["url"], true);
-	console.log(link);
 	var r = "{}";
 	if (link["pathname"] == "/findRecipes") {
 		var recipeUrl = link["query"]["url"];
 		console.log("Scraping " + recipeUrl);
-		var query = require("./db/query");
-		// Do query stuff here after scraping
+		var child = require("child_process");
+		var scraper = child.spawn("python3", ["./scraper/allrecipes.py", recipeUrl]);
+		var scraped = "";
+		scraper.stdout.on("data", function(data) {
+			scraped = scraped + data.toString();
+		});
+		scraper.stdout.on("end", function() {
+			scraped = JSON.parse(scraped);
+			var query = require("./db/query");
+			query.mergeAndSearch(scraped, function (err, data) {
+				res.writeHead(200, {"Content-Type": "application/json"});
+				res.end(JSON.stringify(data));
+				console.log(JSON.stringify(data));
+			});
+		});
 	}
-	res.writeHead(200, {"Content-Type": "application/json"});
-	res.end(r);
 });
 
 server.listen(1337);
