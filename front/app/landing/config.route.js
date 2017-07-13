7885322576be917ddd85f5a257e665071c7cbd1e
lamminade <leahdawson1234@gmail.com>
2017-07-13 01:25:00 -0400

    Changes to front and budgetBytes, added nonowords.txt

---
+++ front/app/landing/config.route.js
@@ -0,0 +1,16 @@
+// set up routing
+(function() {
+  'use strict';
+  
+  angular
+    .module('app.landing')
+    .config(configFunction);
+  
+	configFunction.$inject = ['$routeProvider'];
+		
+	function configFunction($routeProvider) {
+		$routeProvider.when('/', {
+			templateUrl: 'app/landing/landing.html'
+		});	
+  
+})();
