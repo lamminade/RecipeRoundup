6fd3896bb281c8ceeff851989cf92b8de68f080b
lamminade <leahdawson1234@gmail.com>
2017-07-13 01:21:39 -0400

    Updated budgetBytes and added nonowords.txt

--- back/scrapers/budgetBytes.py
+++ back/scrapers/budgetBytes.py
@@ -1,13 +1,18 @@
 import requests
 import sys
 import json
+import string
 from bs4 import BeautifulSoup
 
 def scrape(urlPath):
   # get the html, extract information
-  r = requests.get(urlPath)
-  soup = BeautifulSoup(r.content, "html.parser")
-  ingredientSet = soup.findAll('li', {'class': "ingredient"})
+  try:
+  	r = requests.get(urlPath)
+  	soup = BeautifulSoup(r.content, "html.parser")
+  except:
+  	print("URL error")
+  	sys.exit(-1)	
+  ingredientSet = soup.findAll('span', {'class': "wprm-recipe-ingredient-name"})
   title = soup.find('meta', {'property': 'og:title'})['content']
   ingredientList = []
 
@@ -16,7 +21,44 @@ def scrape(urlPath):
     food = item.contents[0]
     ingredientList.append(food)
 
-  print(json.dumps(dict(recipe=title, ingredients=ingredientList, url=urlPath)))
+  noNoWords = open("nonowords.txt", 'r')
+  noNoWords = noNoWords.read().split(" ")
+
+  # edit ingredients
+  finalIngredients = []
+  for ingredient in ingredientList:
+   	t1 = str.maketrans('', '', string.punctuation)
+   	if isinstance(ingredient, str):
+	   	ingredient = ingredient.lower().translate(t1)
+   		ingredient = ingredient.split(" ")
+   	else:
+   		ingredient = ingredient.string.lower().translate(t1)
+   		ingredient = ingredient.split(" ")
+   	# remove useless / weird words
+   	badWords = []
+   	for word in ingredient:
+   		if word in noNoWords:
+   			badWords.append(word)
+   		if (word == ""):
+   			ingredient.remove(word)
+   	for word in badWords:
+   		ingredient.remove(word)
+
+   	# put edited string back together
+   	finalIngredients.append(' '.join(ingredient))
+
+  # put edited ingredients into json for database
+  print(json.dumps(dict(recipe=title, ingredients=finalIngredients, url=urlPath)))
 
 if __name__ == '__main__':
-	scrape(sys.argv[1])
+	if (len(sys.argv) < 2):
+		print("No recipe supplied")
+		sys.exit(-1)
+	elif (len(sys.argv) > 2):
+		print("Too many arguments supplied")
+		sys.exit(-1)
+	else:
+		scrape(sys.argv[1])
+
+
+#t2 = str.maketrans('', '', " ")
